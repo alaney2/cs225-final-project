c42039f8b62628f5616b1a976fa08c6fca4007d5
YouYou Yu <youyouy2@illinois.edu>
2021-12-01 23:33:16 -0600

    minor issues

--- main.cpp
+++ main.cpp
@@ -1,25 +1,24 @@
 #include "Graph.cpp"
 
 int main(){
-    Graph g = Graph("test.txt");
+    Graph g = Graph("data1.txt");
     string root = g.getRoot();
-    cout << "root " << root << endl;
+    cout << "root-- " << root << endl;
     vector<string> n = g.getNeighbor(root);
     for(auto i : n){
         cout << i<< " || ";
     }
     cout << endl;
-    string search;
     
-    cout << "Enter new url: "<<endl;
-    while (true){
+    while(true){
+        string search;
+        cout << "Enter new url: "<<endl;
         cin >> search;
         vector<string> n2 = g.getNeighbor(search);
         for(auto i : n2){
             cout << i<< " || ";
         }
         cout << endl;
-        cout << "Enter new url or enter 'done'"<<endl;
     }
     return 1;
 
